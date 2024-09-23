d12ddbb43d37f6d62a27f3078440a7e6555aeaa3
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-09-23 19:04:47 -0300

    feat(web):add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import Header from "./components/container/header";
+
 const App = (): JSX.Element => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+          <h2>Create task</h2>
+        </section>
+
+        <section>
+          <h2>Filter</h2>
+        </section>
+
+        <article>
+          <h2>Tasks</h2>
+        </article>
+      </main>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/container/header.tsx
@@ -0,0 +1,17 @@
+import { IoLogoGithub } from "react-icons/io";
+
+function Header(): JSX.Element {
+  return (
+    <header className="header default-size">
+      <nav className="header__nav">
+        <a href="#" target="__blank" className="header__btn-github">
+          <IoLogoGithub />
+          Github
+        </a>
+      </nav>
+      <h1 className="header__title">To-Do</h1>
+    </header>
+  );
+}
+
+export default Header;
