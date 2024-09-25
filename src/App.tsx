93e338d12d687a6ae023e422fdd3de0975b749f0
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-09-25 18:16:17 -0300

    feat(web):add createTask.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import Header from "./components/container/header";
-
+import CreateTask from "./components/createTask";
 const App = (): JSX.Element => {
   return (
     <>
-      <Header />
-      <main>
-        <section>
-          <h2>Create task</h2>
-        </section>
+      <article className="bg-shadow ">
+        <Header />
+        <main className="default-size ">
+          <CreateTask />
 
-        <section>
-          <h2>Filter</h2>
-        </section>
+          <section>
+            <h2>Filter</h2>
+          </section>
 
-        <article>
-          <h2>Tasks</h2>
-        </article>
-      </main>
+          <article>
+            <h2>Tasks</h2>
+          </article>
+        </main>
+      </article>
     </>
   );
 };
---
+++ src/components/createTask.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useTodoContext } from "../context/todo-context";
+
+const CreateTask = () => {
+  let { saveTodo } = useTodoContext();
+  let [task, setTask] = React.useState<string>("");
+  let [category, setCategory] = React.useState<string>("");
+  let [listCategory, setListCategory] = React.useState<string[]>([]);
+
+  const createOptionDatalist = (value: string): void => {
+    setListCategory([...listCategory, value]);
+    const $dataList = document.getElementById("datalist-task");
+    const $option: HTMLOptionElement = document.createElement("option");
+    $option.setAttribute("value", value);
+    $dataList?.appendChild($option);
+  };
+
+  const handlerClick = (): void => {
+    if (!listCategory.includes(typeof category == "string" ? category : "")) {
+      createOptionDatalist(String(category?.trim()));
+    }
+    saveTodo(task, category);
+    setTask("");
+    setCategory("");
+  };
+
+  return (
+    <article>
+      <form
+        className="form-create-task"
+        id="form-create"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handlerClick();
+        }}
+      >
+        <label className="form-create-task__label">
+          Task:
+          <input
+            className="form-create-task__input"
+            type="text"
+            required
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </label>
+
+        <label className="form-create-task__label">
+          Category:
+          <input
+            className="form-create-task__input"
+            type="text"
+            list="datalist-task"
+            required
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+        </label>
+
+        <button className="form-create-task__btn">Add task</button>
+      </form>
+      <datalist id="datalist-task"></datalist>
+    </article>
+  );
+};
+
+export default CreateTask;
